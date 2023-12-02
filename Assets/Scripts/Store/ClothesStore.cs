f8a39667420cc564c3966747472bcf292b7f6387
Paula Picolott <paula_picolott@hotmail.com>
2023-12-02 08:37:41 -0300

    feat: clothes visuals preview

--- Assets/Scripts/Store/ClothesStore.cs
+++ Assets/Scripts/Store/ClothesStore.cs
@@ -26,34 +26,83 @@ public class ClothesStore : MonoBehaviour
     [SerializeField] private AnimatorController[] _hairsAnimators;
     [SerializeField] private AnimatorController[] _underwearAnimators;
     [SerializeField] private AnimatorController[] _outfitsAnimators;
+    [SerializeField] private AnimatorController _nakedAnimator;
 
+    private AnimatorController[][] _animatorsReferences;
+    private string _currentDirection;
 
     public Animator[] FittingRoomAnimator => _fittingRoomAnimator;
 
     private void Awake()
     {
-        _leftArrow.onClick.AddListener(() => SetPreviewAnimation("IdleLeft"));
-        _rightArrow.onClick.AddListener(() => SetPreviewAnimation("IdleRight"));
-        _downArrow.onClick.AddListener(() => SetPreviewAnimation("IdleDown"));
-        _upArrow.onClick.AddListener(() => SetPreviewAnimation("IdleUp"));
+        for (int i = 0; i < _hatsToggles.Length; i++)
+        {
+            int j = i;
+            _hatsToggles[i].onValueChanged.AddListener( delegate { SetPreviewAnimation(_hatsToggles[j].isOn, j, _animatorsReferences[0], 1); });
+        }
+
+        for (int i = 0; i < _hairsToggles.Length; i++)
+        {
+            int j = i;
+            _hairsToggles[i].onValueChanged.AddListener(delegate { SetPreviewAnimation(_hairsToggles[j].isOn, j, _animatorsReferences[1], 2); });
+        }
+
+        for (int i = 0; i < _underwearsToggles.Length; i++)
+        {
+            int j = i;
+            _underwearsToggles[i].onValueChanged.AddListener(delegate { SetPreviewAnimation(_underwearsToggles[j].isOn, j, _animatorsReferences[2], 3); });
+        }
+
+        for (int i = 0; i < _outfitsToggles.Length; i++)
+        {
+            int j = i;
+            _outfitsToggles[i].onValueChanged.AddListener(delegate { SetPreviewAnimation(_outfitsToggles[j].isOn, j, _animatorsReferences[3], 4); });
+        }
+
+        _leftArrow.onClick.AddListener(() => SetPreviewAnimationDirection("IdleLeft"));
+        _rightArrow.onClick.AddListener(() => SetPreviewAnimationDirection("IdleRight"));
+        _downArrow.onClick.AddListener(() => SetPreviewAnimationDirection("IdleDown"));
+        _upArrow.onClick.AddListener(() => SetPreviewAnimationDirection("IdleUp"));
+
+        _animatorsReferences = new AnimatorController[][] { _hatsAnimators , _hairsAnimators , _underwearAnimators , _outfitsAnimators };
+        _currentDirection = "IdleDown";
     }
 
     private void OnDestroy()
     {
+        for (int i = 0; i < _hatsToggles.Length; i++)
+            _hatsToggles[i].onValueChanged.RemoveAllListeners();
+
+        for (int i = 0; i < _hatsToggles.Length; i++)
+            _hairsToggles[i].onValueChanged.RemoveAllListeners();
+
+        for (int i = 0; i < _hatsToggles.Length; i++)
+            _underwearsToggles[i].onValueChanged.RemoveAllListeners();
+
+        for (int i = 0; i < _hatsToggles.Length; i++)
+            _outfitsToggles[i].onValueChanged.RemoveAllListeners();
+
         _leftArrow.onClick.RemoveAllListeners();
         _rightArrow.onClick.RemoveAllListeners();
         _downArrow.onClick.RemoveAllListeners();
         _upArrow.onClick.RemoveAllListeners();
     }
 
-    private void Update()
+    private void SetPreviewAnimation(bool enabled, int animatorIndex, AnimatorController[] animatorReference, int currentAnimatorIndex)
     {
-        Debug.Log(_hatsToggles[0].isOn);
+        if (!enabled)
+            _fittingRoomAnimator[currentAnimatorIndex].runtimeAnimatorController = _nakedAnimator;
+        else
+            _fittingRoomAnimator[currentAnimatorIndex].runtimeAnimatorController = animatorReference[animatorIndex];
+
+        SetPreviewAnimationDirection(_currentDirection);
     }
 
-    private void SetPreviewAnimation(string animationName)
+    private void SetPreviewAnimationDirection(string animationName)
     {
         for (int i = 0; i < _fittingRoomAnimator.Length; i++)
             _fittingRoomAnimator[i].Play(animationName);
+
+        _currentDirection = animationName;
     }
 }
