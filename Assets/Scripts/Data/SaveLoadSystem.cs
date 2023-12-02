4b203461d387f1fb513280bb23dc8df6cb3a9f2b
Paula Picolott <paula_picolott@hotmail.com>
2023-12-02 10:35:14 -0300

    refactor: SaveLoadSystem

--- Assets/Scripts/Data/SaveLoadSystem.cs
+++ Assets/Scripts/Data/SaveLoadSystem.cs
@@ -5,31 +5,30 @@ using UnityEngine.InputSystem;
 public class SaveLoadSystem : MonoBehaviour
 {
     private string _json;
-
-    private Clothes _hatOne;
-    private Clothes _hatTwo;
-
-    private Clothes _femaleHair;
-    private Clothes _maleHair;
-
-    private Clothes _femaleUnderwear;
-    private Clothes _maleUnderwear;
-
-    private Clothes _outfitOne;
-    private Clothes _outfitTwo;
-
     private string[] filePaths;
+
+    public Clothes HatOne;
+    public Clothes HatTwo;
+
+    public Clothes FemaleHair;
+    public Clothes MaleHair;
+
+    public Clothes FemaleUnderwear;
+    public Clothes MaleUnderwear;
+
+    public Clothes OutfitOne;
+    public Clothes OutfitTwo;
 
     private void Awake()
     {
-        _hatOne = new Clothes();
-        _hatTwo = new Clothes();
-        _femaleHair = new Clothes();
-        _maleHair = new Clothes();
-        _femaleUnderwear = new Clothes();
-        _maleUnderwear = new Clothes();
-        _outfitOne = new Clothes();
-        _outfitTwo = new Clothes();
+        HatOne = new Clothes();
+        HatTwo = new Clothes();
+        FemaleHair = new Clothes();
+        MaleHair = new Clothes();
+        FemaleUnderwear = new Clothes();
+        MaleUnderwear = new Clothes();
+        OutfitOne = new Clothes();
+        OutfitTwo = new Clothes();
 
         filePaths = Directory.GetFiles(Application.persistentDataPath + "/", "*.json");
         if (filePaths.Length == 0)
@@ -40,68 +39,68 @@ public class SaveLoadSystem : MonoBehaviour
 
     private void CreateData()
     {
-        _hatOne.ID = "HatOne";
-        _hatOne.FilePath = Application.persistentDataPath + "/HatOneData.json";
-        _hatOne.Price = 10.00d;
-        _hatOne.Equiped = false;
-        _hatOne.Purchased = false;
-        SaveData(_hatOne.FilePath, _hatOne);
+        HatOne.ID = "HatOne";
+        HatOne.FilePath = Application.persistentDataPath + "/HatOneData.json";
+        HatOne.Price = 10.00d;
+        HatOne.Equiped = false;
+        HatOne.Purchased = false;
+        SaveData(HatOne.FilePath, HatOne);
 
-        _hatTwo.ID = "HatTwo";
-        _hatTwo.FilePath = Application.persistentDataPath + "/HatTwoData.json";
-        _hatTwo.Price = 12.00d;
-        _hatTwo.Equiped = false;
-        _hatTwo.Purchased = false;
-        SaveData(_hatTwo.FilePath, _hatTwo);
+        HatTwo.ID = "HatTwo";
+        HatTwo.FilePath = Application.persistentDataPath + "/HatTwoData.json";
+        HatTwo.Price = 12.00d;
+        HatTwo.Equiped = false;
+        HatTwo.Purchased = false;
+        SaveData(HatTwo.FilePath, HatTwo);
 
-        _femaleHair.ID = "FemaleHair";
-        _femaleHair.FilePath = Application.persistentDataPath + "/FemaleHairData.json";
-        _femaleHair.Price = 15.00d;
-        _femaleHair.Equiped = false;
-        _femaleHair.Purchased = false;
-        SaveData(_femaleHair.FilePath, _femaleHair);
+        FemaleHair.ID = "FemaleHair";
+        FemaleHair.FilePath = Application.persistentDataPath + "/FemaleHairData.json";
+        FemaleHair.Price = 15.00d;
+        FemaleHair.Equiped = false;
+        FemaleHair.Purchased = false;
+        SaveData(FemaleHair.FilePath, FemaleHair);
 
-        _maleHair.ID = "MaleHair";
-        _maleHair.FilePath = Application.persistentDataPath + "/MaleHairData.json";
-        _maleHair.Price = 15.00d;
-        _maleHair.Equiped = false;
-        _maleHair.Purchased = false;
-        SaveData(_maleHair.FilePath, _maleHair);
+        MaleHair.ID = "MaleHair";
+        MaleHair.FilePath = Application.persistentDataPath + "/MaleHairData.json";
+        MaleHair.Price = 15.00d;
+        MaleHair.Equiped = false;
+        MaleHair.Purchased = false;
+        SaveData(MaleHair.FilePath, MaleHair);
 
-        _femaleUnderwear.ID = "FemaleUnderwear";
-        _femaleUnderwear.FilePath = Application.persistentDataPath + "/FemaleUnderwearData.json";
-        _femaleUnderwear.Price = 10.00d;
-        _femaleUnderwear.Equiped = false;
-        _femaleUnderwear.Purchased = false;
-        SaveData(_femaleUnderwear.FilePath, _femaleUnderwear);
+        FemaleUnderwear.ID = "FemaleUnderwear";
+        FemaleUnderwear.FilePath = Application.persistentDataPath + "/FemaleUnderwearData.json";
+        FemaleUnderwear.Price = 10.00d;
+        FemaleUnderwear.Equiped = false;
+        FemaleUnderwear.Purchased = false;
+        SaveData(FemaleUnderwear.FilePath, FemaleUnderwear);
 
-        _maleUnderwear.ID = "MaleUnderwear";
-        _maleUnderwear.FilePath = Application.persistentDataPath + "/MaleUnderwearData.json";
-        _maleUnderwear.Price = 8.00d;
-        _maleUnderwear.Equiped = false;
-        _maleUnderwear.Purchased = false;
-        SaveData(_maleUnderwear.FilePath, _maleUnderwear);
+        MaleUnderwear.ID = "MaleUnderwear";
+        MaleUnderwear.FilePath = Application.persistentDataPath + "/MaleUnderwearData.json";
+        MaleUnderwear.Price = 8.00d;
+        MaleUnderwear.Equiped = false;
+        MaleUnderwear.Purchased = false;
+        SaveData(MaleUnderwear.FilePath, MaleUnderwear);
 
-        _outfitOne.ID = "OutfitOne";
-        _outfitOne.FilePath = Application.persistentDataPath + "/OutfitOneData.json";
-        _outfitOne.Price = 30.00d;
-        _outfitOne.Equiped = false;
-        _outfitOne.Purchased = false;
-        SaveData(_outfitOne.FilePath, _outfitOne);
+        OutfitOne.ID = "OutfitOne";
+        OutfitOne.FilePath = Application.persistentDataPath + "/OutfitOneData.json";
+        OutfitOne.Price = 30.00d;
+        OutfitOne.Equiped = false;
+        OutfitOne.Purchased = false;
+        SaveData(OutfitOne.FilePath, OutfitOne);
 
-        _outfitTwo.ID = "OutfitTwo";
-        _outfitTwo.FilePath = Application.persistentDataPath + "/OutfitTwoData.json";
-        _outfitTwo.Price = 28.00d;
-        _outfitTwo.Equiped = false;
-        _outfitTwo.Purchased = false;
-        SaveData(_outfitTwo.FilePath, _outfitTwo);
+        OutfitTwo.ID = "OutfitTwo";
+        OutfitTwo.FilePath = Application.persistentDataPath + "/OutfitTwoData.json";
+        OutfitTwo.Price = 28.00d;
+        OutfitTwo.Equiped = false;
+        OutfitTwo.Purchased = false;
+        SaveData(OutfitTwo.FilePath, OutfitTwo);
     }
 
     private void Update()
     {
         // Temporary - Debug:
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
-            UpdateData(_outfitTwo.FilePath, _outfitTwo, true, true);
+            UpdateData(OutfitTwo.FilePath, OutfitTwo, true, true);
     }
 
     private void UpdateData(string filePath, Clothes clothes, bool purchased, bool equiped)
@@ -121,27 +120,27 @@ public class SaveLoadSystem : MonoBehaviour
     private void LoadData()
     {
         _json = File.ReadAllText(filePaths[0]);
-        _hatOne = JsonUtility.FromJson<Clothes>(_json);
+        HatOne = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[1]);
-        _hatTwo = JsonUtility.FromJson<Clothes>(_json);
+        HatTwo = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[2]);
-        _femaleHair = JsonUtility.FromJson<Clothes>(_json);
+        FemaleHair = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[3]);
-        _maleHair = JsonUtility.FromJson<Clothes>(_json);
+        MaleHair = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[4]);
-        _femaleUnderwear = JsonUtility.FromJson<Clothes>(_json);
+        FemaleUnderwear = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[5]);
-        _maleUnderwear = JsonUtility.FromJson<Clothes>(_json);
+        MaleUnderwear = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[6]);
-        _outfitOne = JsonUtility.FromJson<Clothes>(_json);
+        OutfitOne = JsonUtility.FromJson<Clothes>(_json);
 
         _json = File.ReadAllText(filePaths[7]);
-        _outfitTwo = JsonUtility.FromJson<Clothes>(_json);
+        OutfitTwo = JsonUtility.FromJson<Clothes>(_json);
     }
 }
