bb56724e114e041444f00194c314c60e73d20b37
Paula Picolott <paula_picolott@hotmail.com>
2023-12-02 02:09:42 -0300

    feat: player movement

--- Assets/Scripts/Player/Inputs/Movement.cs
+++ Assets/Scripts/Player/Inputs/Movement.cs
@@ -1,23 +1,68 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private InputActionReference _movementInput;
+    [SerializeField] private Rigidbody2D _playerRigidBody;
     [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private SpriteRenderer _playerSpriteRenderer;
+    [SerializeField] private List<Sprite> _idleSprites;
+    [SerializeField] private float _speed;
 
-    void Update()
+    private Vector2 _direction;
+
+    private void Awake()
     {
-        if (Keyboard.current.aKey.wasPressedThisFrame)
-            _playerAnimator.SetInteger("WalkHorizontal", -1);
+        _speed = 1.5f;
+        _movementInput.action.started += GetDirection;
+    }
 
-        if (Keyboard.current.dKey.wasPressedThisFrame)
-            _playerAnimator.SetInteger("WalkHorizontal", 1);
+    private void OnDestroy()
+    {
+        _movementInput.action.started -= GetDirection;
+    }
 
-        if (Keyboard.current.wKey.wasPressedThisFrame)
-            _playerAnimator.SetInteger("WalkVertical", 1);
+    private void FixedUpdate()
+    {
+        Move();
+        AnimateMovement();
+    }
 
-        if (Keyboard.current.sKey.wasPressedThisFrame)
-            _playerAnimator.SetInteger("WalkVertical", -1);
+    private void GetDirection(InputAction.CallbackContext context)
+    {
+        _direction = context.ReadValue<Vector2>();
+    }
 
+    private void Move()
+    {
+        _playerRigidBody.velocity = _movementInput.action.ReadValue<Vector2>() * _speed;
+    }
+
+    private void AnimateMovement()
+    {
+        if (_movementInput.action.ReadValue<Vector2>() != Vector2.zero)
+        {
+            if (_movementInput.action.ReadValue<Vector2>().x < 0)
+                _playerAnimator.Play("WalkLeft");
+            else if (_movementInput.action.ReadValue<Vector2>().x > 0)
+                _playerAnimator.Play("WalkRight");
+            else if (_movementInput.action.ReadValue<Vector2>().y < 0)
+                _playerAnimator.Play("WalkDown");
+            else if (_movementInput.action.ReadValue<Vector2>().y > 0)
+                _playerAnimator.Play("WalkUp");
+        }
+        else
+        {
+            if (_direction.x < 0)
+                _playerAnimator.Play("IdleLeft");
+            else if (_direction.x > 0)
+                _playerAnimator.Play("IdleRight");
+            else if (_direction.y < 0)
+                _playerAnimator.Play("IdleDown");
+            else if (_direction.y > 0)
+                _playerAnimator.Play("IdleUp");
+        }
     }
 }
